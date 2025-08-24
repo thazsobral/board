842e120b18680c1d0557284647e6454bb3ad1fcf
Thaz Sobral <thazsobral@gmail.com>
2025-08-24 13:15:32 -0300

    fix connection with database

--- board/src/main/java/me/thazsobral/Main.java
+++ board/src/main/java/me/thazsobral/Main.java
@@ -7,11 +7,14 @@ import me.thazsobral.persistence.migrations.MigrationStrategy;
 import static me.thazsobral.persistence.configs.ConnectionConfig.getConnection;
 
 public class Main {
-    public static void main(String[] args) throws SQLException  {
+    public static void main(String[] args) {
         try (
             var connection = getConnection();
         ) {
             new MigrationStrategy(connection).executeMigration();
+            System.out.println("Ok");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
